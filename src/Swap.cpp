1ca7c0fb19ad5144bdb246deba127353c6497663
almui <alarluci210604@gmail.com>
2025-07-05 00:31:11 -0300

    swap funciona

--- src/Swap.cpp
+++ src/Swap.cpp
@@ -7,27 +7,27 @@ Solution route_swap(const Solution& solution_original) {
     const VRPLIBReader& instancia = solution.getInstancia();
     const vector<int>& demandas = instancia.getDemands();
     int capacidad = instancia.getCapacity();
-
+    
     bool hay_mejora = true;
 
     while (hay_mejora) {
         hay_mejora = false;
         double mejor_costo = calcularCostoTotal(solution);
         vector<vector<int>> rutas = solution.getRutas();
+        Solution mejor_solucion = solution;
 
-        bool mejora_encontrada = false;
 
-        for (int r1 = 0; r1 < rutas.size() && !mejora_encontrada; ++r1) {
-            for (int r2 = 0; r2 < rutas.size() && !mejora_encontrada; ++r2) {
+        for (int r1 = 0; r1 < rutas.size() ; ++r1) {
+            for (int r2 = 0; r2 < rutas.size() ; ++r2) {
                 if (r1 == r2) continue;
 
-                for (int i = 1; i < rutas[r1].size() - 1 && !mejora_encontrada; ++i) {
-                    for (int j = 1; j < rutas[r2].size() - 1 && !mejora_encontrada; ++j) {
+                for (int i = 1; i < rutas[r1].size() - 1 ; ++i) {
+                    for (int j = 1; j < rutas[r2].size() - 1 ; ++j) {
 
                         int c1 = rutas[r1][i];
                         int c2 = rutas[r2][j];
-                        int demanda_c1 = demandas[c1 + 1];
-                        int demanda_c2 = demandas[c2 + 1];
+                        int demanda_c1 = demandas[c1];
+                        int demanda_c2 = demandas[c2];
 
                         int nueva_d1 = solution.getDemandas()[r1] - demanda_c1 + demanda_c2;
                         int nueva_d2 = solution.getDemandas()[r2] - demanda_c2 + demanda_c1;
@@ -48,15 +48,21 @@ Solution route_swap(const Solution& solution_original) {
                         double nuevo_costo = calcularCostoTotal(copia);
 
                         if (nuevo_costo < mejor_costo) {
-                            solution = copia;
+                            mejor_solucion = copia;
+
                             mejor_costo = nuevo_costo;
                             hay_mejora = true;
-                            mejora_encontrada = true;
+
                         }
                     }
                 }
             }
         }
+        if (hay_mejora){
+            solution = mejor_solucion;
+
+        }
+
     }
 
     return solution;
